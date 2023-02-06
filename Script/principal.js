dc69c81750fec38a57c1d4b9711122e9292bbf08
new19 <juanma.a0190@gmail.com>
2023-02-06 20:07:54 -0300

    Se sube el primer challenge de Alura a GitHub

---
+++ Script/principal.js
@@ -0,0 +1,8 @@
+function changeDisplay(){
+   document.getElementById("img-notfound").style.display = "none";
+    document.getElementById("title-notfound").style.display = "none";
+    document.getElementById("p-notfound").style.display = "none";
+   
+    document.getElementById("area-ouput").style.display = "block";
+    document.getElementById("btn-copiar").style.display = "block";
+}
